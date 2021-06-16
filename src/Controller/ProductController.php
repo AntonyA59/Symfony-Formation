ed2e05c53b2cd28190b26a9397e26f4f8764a627
whiitewolf59 <83072947+whiitewolf59@users.noreply.github.com>
2021-06-16 11:25:49 +0200

    Travail sur la barre de navigation

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -10,9 +10,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 use Symfony\Component\String\Slugger\SluggerInterface;
-
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
