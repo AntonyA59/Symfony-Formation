6d612963eae3475818906cceb29129419584b96a
whiitewolf59 <83072947+whiitewolf59@users.noreply.github.com>
2021-06-17 09:39:08 +0200

    Introduction à la sécurité à notre application

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,11 +6,15 @@ use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CategoryController extends AbstractController
 {
@@ -59,9 +63,17 @@ class CategoryController extends AbstractController
     /**
      * @Route("/admin/category/{id}/edit", name="category_edit")
      */
-    public function edit($id, CategoryRepository $categoryRepository,Request $request,SluggerInterface $slugger, EntityManagerInterface $em): Response
+    public function edit($id, CategoryRepository $categoryRepository,Request $request,SluggerInterface $slugger, EntityManagerInterface $em, Security $security): Response
     {
+        
         $category= $categoryRepository->find($id);
+
+        if(!$category)
+        {
+            throw new NotFoundHttpException("Cette catégorie n'existe pas");
+        }
+        
+
         $form = $this->createForm(CategoryType::class, $category);
 
         $form->handleRequest($request);
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,48 +2,73 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Category;
 use Faker\Factory;
+use App\Entity\User;
 use App\Entity\Product;
+use App\Entity\Category;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
     protected $slugger;
+    protected $encoder;
 
-    public function __construct(SluggerInterface $slugger)
+    public function __construct(SluggerInterface $slugger, UserPasswordEncoderInterface $encoder)
     {
         $this->slugger = $slugger;
+        $this->encoder = $encoder;
     }
     public function load(ObjectManager $manager)
     {
-        
-        
-        $faker= Factory::create('fr_FR');
+
+
+        $faker = Factory::create('fr_FR');
         $faker->addProvider(new \Liior\Faker\Prices($faker));
         $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
         $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
+
         
-        for($c = 0; $c < 3; $c++)
-        {
-            $category= new Category;
+
+        $admin = new User;
+        $hash= $this->encoder->encodePassword($admin, "password");
+        $admin
+            ->setEmail("admin@gmail.com")
+            ->setPassword($hash)
+            ->setFullName("Admin")
+            ->setRoles(["ROLE_ADMIN"]);
+
+        $manager->persist($admin);
+
+        for ($u = 0; $u < 5; $u++) {
+            $user = new User;
+            $hash= $this->encoder->encodePassword($admin, "password");
+            $user
+                ->setEmail("User$u@gmail.com")
+                ->setFullName($faker->name())
+                ->setPassword($hash);
+                $manager->persist($user);
+        }
+        
+
+        for ($c = 0; $c < 3; $c++) {
+            $category = new Category;
             $category
-            ->setName($faker->department())
-            ->setSlug(strtolower($this->slugger->slug($category->getName())));
+                ->setName($faker->department())
+                ->setSlug(strtolower($this->slugger->slug($category->getName())));
             $manager->persist($category);
 
-            for($p = 0; $p < mt_rand(15, 20); $p++)
-            {
+            for ($p = 0; $p < mt_rand(15, 20); $p++) {
                 $product = new Product;
                 $product
-                ->setName($faker->productName())
-                ->setPrice($faker->price(4000, 20000))
-                ->setSlug(strtolower($this->slugger->slug($product->getName())))
-                ->setCategory($category)
-                ->setShortDescription($faker->paragraph())
-                ->setMainPicture($faker->imageUrl(400, 400, true));
+                    ->setName($faker->productName())
+                    ->setPrice($faker->price(4000, 20000))
+                    ->setSlug(strtolower($this->slugger->slug($product->getName())))
+                    ->setCategory($category)
+                    ->setShortDescription($faker->paragraph())
+                    ->setMainPicture($faker->imageUrl(400, 400, true));
                 $manager->persist($product);
             }
         }
